5708a9af38ba0f7b180166a24a5946662e4bdbdf
Elliott Clark <elliott@fb.com>
2015-11-30 01:41:58 -0800

    More tests for hand rank

--- core/hand_test.cpp
+++ core/hand_test.cpp
@@ -1,8 +1,10 @@
 #include "core/hand.h"
 #include "core/deck.h"
 #include "gtest/gtest.h"
+#include <iostream>
 
 using namespace yg;
+using namespace std;
 
 TEST(HandTest, testAddCard) {
   Hand h;
@@ -131,72 +133,101 @@ TEST(HandTest, testFour) {
   EXPECT_LT(h.rank(), 156);
 }
 TEST(HandTest, testFull) {
-  Hand h;
-  Card c1{kTwo, kHearts};
-  Card c2{kTwo, kClubs};
-  Card c3{kTwo, kDiamonds};
-  Card c4{kThree, kSpades};
-  Card c5{kThree, kClubs};
+  for (uint16_t v1 : kValues) {
+    for (uint16_t v2 : kValues) {
+      if (v1 == v2)
+        continue;
+      Hand h;
+      Card c1{v1, kHearts};
+      Card c2{v1, kClubs};
+      Card c3{v1, kDiamonds};
+      Card c4{v2, kSpades};
+      Card c5{v2, kClubs};
 
-  h.add_card(c1);
-  h.add_card(c2);
-  h.add_card(c3);
-  h.add_card(c4);
-  h.add_card(c5);
+      h.add_card(c1);
+      h.add_card(c2);
+      h.add_card(c3);
+      h.add_card(c4);
+      h.add_card(c5);
 
-  EXPECT_GE(h.rank(), 1660);
-  EXPECT_LT(h.rank(), 3220);
+      EXPECT_GE(h.rank(), 1660);
+      EXPECT_LT(h.rank(), 3220);
+    }
+  }
 }
 TEST(HandTest, testThree) {
-  Hand h;
-  Card c1{kTwo, kHearts};
-  Card c2{kTwo, kClubs};
-  Card c3{kTwo, kDiamonds};
-  Card c4{kThree, kSpades};
-  Card c5{kFour, kClubs};
+  for (uint16_t v1 : kValues) {
+    for (uint16_t v2 : kValues) {
+      if (v1 == v2)
+        continue;
+      for (uint16_t v3 : kValues) {
+        if (v1 == v3 || v2 == v3)
+          continue;
+        Hand h;
+        Card c1{v1, kHearts};
+        Card c2{v1, kClubs};
+        Card c3{v1, kDiamonds};
+        Card c4{v2, kSpades};
+        Card c5{v3, kClubs};
 
-  h.add_card(c1);
-  h.add_card(c2);
-  h.add_card(c3);
-  h.add_card(c4);
-  h.add_card(c5);
+        h.add_card(c1);
+        h.add_card(c2);
+        h.add_card(c3);
+        h.add_card(c4);
+        h.add_card(c5);
 
-  EXPECT_GE(h.rank(), 16090);
-  EXPECT_LT(h.rank(), 24670);
+        EXPECT_GE(h.rank(), 16090);
+        EXPECT_LT(h.rank(), 24670);
+      }
+    }
+  }
 }
 TEST(HandTest, testTwoPair) {
-  Hand h;
-  Card c1{kTwo, kHearts};
-  Card c2{kTwo, kClubs};
-  Card c3{kAce, kDiamonds};
-  Card c4{kThree, kSpades};
-  Card c5{kThree, kClubs};
+  for (uint16_t v1 : kValues) {
+    for (uint16_t v2 : kValues) {
+      if (v1 == v2)
+        continue;
+      for (uint16_t v3 : kValues) {
+        if (v1 == v3 || v2 == v3)
+          continue;
+        Hand h;
+        Card c1{v1, kHearts};
+        Card c2{v1, kClubs};
+        Card c3{v2, kDiamonds};
+        Card c4{v2, kSpades};
+        Card c5{v3, kClubs};
 
-  h.add_card(c1);
-  h.add_card(c2);
-  h.add_card(c3);
-  h.add_card(c4);
-  h.add_card(c5);
+        h.add_card(c1);
+        h.add_card(c2);
+        h.add_card(c3);
+        h.add_card(c4);
+        h.add_card(c5);
 
-  EXPECT_GE(h.rank(), 246700);
-  EXPECT_LT(h.rank(), 332500);
+        EXPECT_GE(h.rank(), 246700);
+        EXPECT_LT(h.rank(), 332500);
+      }
+    }
+  }
 }
 TEST(HandTest, testPair) {
-  Hand h;
-  Card c1{kTwo, kHearts};
-  Card c2{kTwo, kClubs};
-  Card c3{kAce, kDiamonds};
-  Card c4{kThree, kSpades};
-  Card c5{kSeven, kClubs};
+  for (uint i = 0; i < kValues.size(); i++) {
+    Hand h;
+    Card c1{kValues[i], kHearts};
+    Card c2{kValues[i], kDiamonds};
 
-  h.add_card(c1);
-  h.add_card(c2);
-  h.add_card(c3);
-  h.add_card(c4);
-  h.add_card(c5);
+    Card c3{kValues[(i + 1) % kValues.size()], kSpades};
+    Card c4{kValues[(i + 2) % kValues.size()], kSpades};
+    Card c5{kValues[(i + 3) % kValues.size()], kSpades};
 
-  EXPECT_GE(h.rank(), 332500);
-  EXPECT_LT(h.rank(), 618500);
+    h.add_card(c1);
+    h.add_card(c2);
+    h.add_card(c3);
+    h.add_card(c4);
+    h.add_card(c5);
+
+    EXPECT_GE(h.rank(), 332500);
+    EXPECT_LT(h.rank(), 618500);
+  }
 }
 TEST(HandTest, testHighCard) {
   Hand h;
